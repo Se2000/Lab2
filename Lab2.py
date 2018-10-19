3945854ea27ee5c2518353f3939a229bcce2a298
Pece <yemeth44@gmail.com>
2018-10-20 01:31:14 +0200

    j

--- Lab2.py
+++ Lab2.py
@@ -15,7 +15,7 @@
 #
 # 3.
 #
-# wiadomosc = input("Wpisz wiadomość do zaszyfrowania: ")
+# wiadomosc = input("Wpisz wiadomość do zaszyfrowania: ")ś
 #
 # klucz = 3
 #
@@ -32,20 +32,26 @@
 #
 # print("Zaszyfrowana wiadomość:\n", szyfrowanie(wiadomosc))
 #
-for x in range(65, 91):
-    print(chr(x) + chr(x + 32), end="")
-    if x == 90:
-        print("")
-        for x in range(65, 91):
-            print(chr(x + 32) + chr(x), end="")
+# for x in range(65, 91):
+#     print(chr(x) + chr(x + 32), end="")
+#     if x == 90:
+#         print("")
+#         for x in range(65, 91):
+#             print(chr(x + 32) + chr(x), end="")
 
-# bok_a = int(input("Podaj bok a: "))
-# bok_b = int(input("Podaj bok b: "))
-# bok_c = int(input("Podaj bok c: "))
-#
-# if ((bok_a + bok_b) > bok_c) & ((bok_a + bok_c) > bok_b) & ((bok_b + bok_c) > bok_a):
-#     print("Da się zbudować trójkąt z podancyh boków.")
-#
-#     nadluzszy_bok = max(bok_a, bok_b, bok_c)
-#     if
-# jjggggjjjj
+bok_a = int(input("Podaj bok a: "))
+bok_b = int(input("Podaj bok b: "))
+bok_c = int(input("Podaj bok c: "))
+
+if ((bok_a + bok_b) > bok_c) & ((bok_a + bok_c) > bok_b) & ((bok_b + bok_c) > bok_a):
+    print("Da się zbudować trójkąt z podancyh boków.")
+    trojkat = [bok_a, bok_b, bok_c]
+    nadluzszy_bok = max(trojkat)
+    trojkat.remove(max(trojkat))
+    if ((trojkat[0] ** 2) + (trojkat[1] ** 2) == nadluzszy_bok ** 2):
+        print("Trójkąt prostokątny.")
+
+    print("Pole trójkąta wynosi: ", ((bok_a + bok_b + bok_c) / 2))
+    print("Obwód trójkąta wynosi: ", (bok_a + bok_b + bok_c))
+else:
+    print("Nie da się zbudować trójkąta.")
