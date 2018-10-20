45093bb1ee170a64201a98777daef1b5070258bb
Pc <yemeth44@gmail.com>
2018-10-20 19:20:57 +0200

    j

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,4 @@
-# 1.
+# 1.1
 #
 # liczba_porzadkowa = int(input("Podaj liczbę porządkową: "))
 # liczba_koncowa = int(input("Podaj liczbę końcową: "))
@@ -7,13 +7,13 @@
 # for x in range(liczba_porzadkowa, liczba_koncowa, wielkosc_odstepu):
 #     print(x, end=" ")
 #
-# 2.
+# 1.2
 #
 # komunikat = input("Wpisz swój komunikat: ")
 #
 # print(komunikat[::-1])
 #
-# 3.
+# 2.3
 #
 # wiadomosc = input("Wpisz wiadomość do zaszyfrowania: ")ś
 #
@@ -32,6 +32,27 @@
 #
 # print("Zaszyfrowana wiadomość:\n", szyfrowanie(wiadomosc))
 #
+
+# 3.1
+# bok_a = int(input("Podaj bok a: "))
+# bok_b = int(input("Podaj bok b: "))
+# bok_c = int(input("Podaj bok c: "))
+#
+# if ((bok_a + bok_b) > bok_c) & ((bok_a + bok_c) > bok_b) & ((bok_b + bok_c) > bok_a):
+#     print("Da się zbudować trójkąt z podancyh boków.")
+#     trojkat = [bok_a, bok_b, bok_c]
+#     nadluzszy_bok = max(trojkat)
+#     trojkat.remove(max(trojkat))
+#     if ((trojkat[0] ** 2) + (trojkat[1] ** 2) == nadluzszy_bok ** 2):
+#         print("Trójkąt prostokątny.")
+#
+#     print("Pole trójkąta wynosi: ", ((bok_a + bok_b + bok_c) / 2))
+#     print("Obwód trójkąta wynosi: ", (bok_a + bok_b + bok_c))
+# else:
+#     print("Nie da się zbudować trójkąta.")
+
+# 3.2
+
 # for x in range(65, 91):
 #     print(chr(x) + chr(x + 32), end="")
 #     if x == 90:
@@ -39,19 +60,30 @@
 #         for x in range(65, 91):
 #             print(chr(x + 32) + chr(x), end="")
 
-bok_a = int(input("Podaj bok a: "))
-bok_b = int(input("Podaj bok b: "))
-bok_c = int(input("Podaj bok c: "))
+import random
 
-if ((bok_a + bok_b) > bok_c) & ((bok_a + bok_c) > bok_b) & ((bok_b + bok_c) > bok_a):
-    print("Da się zbudować trójkąt z podancyh boków.")
-    trojkat = [bok_a, bok_b, bok_c]
-    nadluzszy_bok = max(trojkat)
-    trojkat.remove(max(trojkat))
-    if ((trojkat[0] ** 2) + (trojkat[1] ** 2) == nadluzszy_bok ** 2):
-        print("Trójkąt prostokątny.")
+n = int(input("Podaj ilość liczb: "))
 
-    print("Pole trójkąta wynosi: ", ((bok_a + bok_b + bok_c) / 2))
-    print("Obwód trójkąta wynosi: ", (bok_a + bok_b + bok_c))
-else:
-    print("Nie da się zbudować trójkąta.")
+lista_liczb = []
+for x in range(0, n):
+    lista_liczb.append(random.randint(0, 99))
+
+print("Lista z indeksami.")
+for x, y in enumerate(lista_liczb):
+    print("[", x, "]", y, end=" ")
+
+print("\nLista w odwróconej kolejności.")
+for x in reversed(lista_liczb):
+    print(x, end=" ")
+
+print("\nLista uporządkowana.")
+for x in sorted((lista_liczb)):
+    print(x, end=" ")
+
+liczba_do_usuniecia = int(input("\nJaką liczbę usunąć z listy."))
+lista_liczb.remove(liczba_do_usuniecia)
+
+# if lista_liczb.index(liczba_do_usuniecia)==True:
+#     lista_liczb.remove(liczba_do_usuniecia)
+# else:
+#     print("Brak elementu w liście")
--- pingpong.py
+++ pingpong.py
@@ -1,102 +1,130 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
+
 pygame.init()
-FPS = 30
-fpsZegar = pygame.time.Clock()
+
 OKNOGRY_SZER = 800
 OKNOGRY_WYS = 400
-
-OKNOGRY = pygame.display.set_mode((OKNOGRY_SZER,OKNOGRY_WYS), 0, 32)
-pygame.display.set_caption('Prosty Pong')
 LT_BLUE = (230, 255, 255)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+
+oknogry = pygame.display.set_mode((OKNOGRY_SZER, OKNOGRY_WYS), 0, 32)
+pygame.display.set_caption('Prosty Pong')
 
 PALETKA_SZER = 100
 PALETKA_WYS = 20
-PALETKA_1_POZ = (350, 360) # początkowa pozycja paletki gracza
-paletka1_obr = pygame.Surface([PALETKA_SZER, PALETKA_WYS])
-paletka1_obr.fill(BLUE)
-paletka1_prost = paletka1_obr.get_rect()
+BLUE = (0, 0, 255)
+PALETKA_1_POZ = (350, 360)
+paletka1 = pygame.Surface([PALETKA_SZER, PALETKA_WYS])
+paletka1.fill(BLUE)
+paletka1_prost = paletka1.get_rect()
 paletka1_prost.x = PALETKA_1_POZ[0]
 paletka1_prost.y = PALETKA_1_POZ[1]
 
-AI_PREDKOSC = 3
+P_SZER = 20
+P_WYS = 20
+P_PREDKOSC_X = 4
+P_PREDKOSC_Y = 4
+GREEN = (0, 255, 0)
+pilka = pygame.Surface([P_SZER, P_WYS], pygame.SRCALPHA, 32).convert_alpha()
+pygame.draw.ellipse(pilka, GREEN, [0, 0, P_SZER, P_WYS])
+pilka_prost = pilka.get_rect()
+pilka_prost.x = OKNOGRY_SZER / 2
+pilka_prost.y = OKNOGRY_WYS / 2
 
-PILKA_SZER = 20
-PILKA_WYS = 20
-PILKA_PREDKOSC_X = 6
-PILKA_PREDKOSC_Y = 6
-pilka_obr = pygame.Surface([PILKA_SZER, PILKA_WYS], pygame.SRCALPHA,32).convert_alpha()
-pygame.draw.ellipse(pilka_obr, GREEN, [0, 0, PILKA_SZER, PILKA_WYS])
-pilka_prost = pilka_obr.get_rect()
-pilka_prost.x = OKNOGRY_SZER/2
-pilka_prost.y = OKNOGRY_WYS/2
+FPS = 30
+fpsClock = pygame.time.Clock()
 
-GRACZ_1_PKT = '0'
-GRACZ_2_PKT = '0'
-# fontObj = pygame.font.Font('pingpong.ttf', 64)
-fontObj = pygame.font.SysFont('arial', 20)
+RED = (255, 0, 0)
+PALETKA_AI_POZ = (350, 20)
+paletkaAI = pygame.Surface([PALETKA_SZER, PALETKA_WYS])
+paletkaAI.fill(RED)
+paletkaAI_prost = paletkaAI.get_rect()
+paletkaAI_prost.x = PALETKA_AI_POZ[0]
+paletkaAI_prost.y = PALETKA_AI_POZ[1]
+PREDKOSC_AI = 5
 
-def drukuj_punkty_p1():
-    tekst_obr1 = fontObj.render(GRACZ_1_PKT, True, (0,0,0))
-    tekst_prost1 = tekst_obr1.get_rect()
-    tekst_prost1.center = (OKNOGRY_SZER/2, OKNOGRY_WYS*0.75)
-    OKNOGRY.blit(tekst_obr1, tekst_prost1)
+PKT_1 = '0'
+PKT_AI = '0'
+fontObj = pygame.font.SysFont('arial', 64)
+
+def drukuj_punkty1():
+    tekst1 = fontObj.render(PKT_1, True, (0, 0, 0))
+    tekst_prost1 = tekst1.get_rect()
+    tekst_prost1.center = (OKNOGRY_SZER / 2, OKNOGRY_WYS * 0.75)
+    oknogry.blit(tekst1, tekst_prost1)
+
+
+def drukuj_punktyAI():
+    tekstAI = fontObj.render(PKT_AI, True, (0, 0, 0))
+    tekst_prostAI = tekstAI.get_rect()
+    tekst_prostAI.center = (OKNOGRY_SZER / 2, OKNOGRY_WYS / 4)
+    oknogry.blit(tekstAI, tekst_prostAI)
+
+pygame.key.set_repeat(50, 25)
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    if event.type == MOUSEMOTION:
-        myszaX, myszaY = event.pos
 
-przesuniecie = myszaX-(PALETKA_SZER/2)
-if przesuniecie > OKNOGRY_SZER-PALETKA_SZER:
-    przesuniecie = OKNOGRY_SZER-PALETKA_SZER
-if przesuniecie < 0:
-    przesuniecie = 0
-paletka1_prost.x = przesuniecie
+        if event.type == MOUSEMOTION:
+            myszaX, myszaY = event.pos
 
-if pilka_prost.centerx > paletka2_prost.centerx:
-    paletka2_prost.x += AI_PREDKOSC
-elif pilka_prost.centerx <paletka2_prost.centerx:
-    paletka2_prost.x -= AI_PREDKOSC
-pilka_prost.x += PILKA_PREDKOSC_X
-pilka_prost.y += PILKA_PREDKOSC_Y
+            przesuniecie = myszaX - (PALETKA_SZER / 2)
 
-if pilka_prost.right >= OKNOGRY_SZER:
-    PILKA_PREDKOSC_X *= -1
-if pilka_prost.left <= 0:
-    PILKA_PREDKOSC_X *= -1
+            if przesuniecie > OKNOGRY_SZER - PALETKA_SZER:
+                przesuniecie = OKNOGRY_SZER - PALETKA_SZER
+            if przesuniecie < 0:
+                przesuniecie = 0
+            paletka1_prost.x = przesuniecie
 
-if pilka_prost.colliderect(paletka1_prost):
-    PILKA_PREDKOSC_Y *= -1
-    pilka_prost.bottom = paletka1_prost.top
-if pilka_prost.colliderect(paletka2_prost):
-    PILKA_PREDKOSC_Y *= -1
-    pilka_prost.top = paletka2_prost.bottom
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                paletka1_prost.x -= 5
+                if paletka1_prost.x < 0:
+                    paletka1_prost.x = 0
+            if event.key == pygame.K_RIGHT:
+                paletka1_prost.x += 5
+                if paletka1_prost.x > OKNOGRY_SZER - PALETKA_SZER:
+                    paletka1_prost.x = OKNOGRY_SZER - PALETKA_SZER
 
-if pilka_prost.top <= 0:
-    pilka_prost.x = OKNOGRY_SZER/2
-    pilka_prost.y = OKNOGRY_WYS/2
-    GRACZ_1_PKT = str(int(GRACZ_1_PKT)+1)
-if pilka_prost.bottom >= OKNOGRY_WYS:
-    pilka_prost.x = OKNOGRY_SZER/2
-    pilka_prost.y = OKNOGRY_WYS/2
-    GRACZ_2_PKT = str(int(GRACZ_2_PKT)+1)
+    pilka_prost.move_ip(P_PREDKOSC_X, P_PREDKOSC_Y)
 
-OKNOGRY.fill(LT_BLUE)
-drukuj_punkty_p1()
-drukuj_punkty_p2()
-OKNOGRY.blit(paletka1_obr, paletka1_prost)
-OKNOGRY.blit(paletka2_obr, paletka2_prost)
-OKNOGRY.blit(pilka_obr, pilka_prost)
-pygame.display.update()
-fpsZegar.tick(FPS)
+    if pilka_prost.right >= OKNOGRY_SZER:
+        P_PREDKOSC_X *= -1
+    if pilka_prost.left <= 0:
+        P_PREDKOSC_X *= -1
 
+    if pilka_prost.top <= 0:
+        pilka_prost.x = OKNOGRY_SZER / 2
+        pilka_prost.y = OKNOGRY_WYS / 2
+        PKT_1 = str(int(PKT_1) + 1)
 
+    if pilka_prost.bottom >= OKNOGRY_WYS:
+        pilka_prost.x = OKNOGRY_SZER / 2
+        pilka_prost.y = OKNOGRY_WYS / 2
+        PKT_AI = str(int(PKT_AI) + 1)
 
+    if pilka_prost.centerx > paletkaAI_prost.centerx:
+        paletkaAI_prost.x += PREDKOSC_AI
+    elif pilka_prost.centerx < paletkaAI_prost.centerx:
+        paletkaAI_prost.x -= PREDKOSC_AI
+
+    if pilka_prost.colliderect(paletkaAI_prost):
+        P_PREDKOSC_Y *= -1
+        pilka_prost.top = paletkaAI_prost.bottom
+
+    if pilka_prost.colliderect(paletka1_prost):
+        P_PREDKOSC_Y *= -1
+        pilka_prost.bottom = paletka1_prost.top
+
+    oknogry.fill(LT_BLUE)
+    drukuj_punkty1()
+    drukuj_punktyAI()
+    oknogry.blit(paletka1, paletka1_prost)
+    oknogry.blit(paletkaAI, paletkaAI_prost)
+    oknogry.blit(pilka, pilka_prost)
+    pygame.display.update()
+    fpsClock.tick(FPS)
