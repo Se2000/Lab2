b339d28796a91124e7175b2e70f0fcb5513228c2
Pc <yemeth44@gmail.com>
2018-10-20 20:11:57 +0200

    j

--- Lab2.py
+++ Lab2.py
@@ -19,7 +19,6 @@
 #
 # klucz = 3
 #
-#
 # def szyfrowanie(wiadomosc):
 #     zaszyfrowana_wiadomosc = ""
 #     for x in range(len(wiadomosc)):
@@ -34,22 +33,22 @@
 #
 
 # 3.1
-# bok_a = int(input("Podaj bok a: "))
-# bok_b = int(input("Podaj bok b: "))
-# bok_c = int(input("Podaj bok c: "))
-#
-# if ((bok_a + bok_b) > bok_c) & ((bok_a + bok_c) > bok_b) & ((bok_b + bok_c) > bok_a):
-#     print("Da się zbudować trójkąt z podancyh boków.")
-#     trojkat = [bok_a, bok_b, bok_c]
-#     nadluzszy_bok = max(trojkat)
-#     trojkat.remove(max(trojkat))
-#     if ((trojkat[0] ** 2) + (trojkat[1] ** 2) == nadluzszy_bok ** 2):
-#         print("Trójkąt prostokątny.")
-#
-#     print("Pole trójkąta wynosi: ", ((bok_a + bok_b + bok_c) / 2))
-#     print("Obwód trójkąta wynosi: ", (bok_a + bok_b + bok_c))
-# else:
-#     print("Nie da się zbudować trójkąta.")
+bok_a = int(input("Podaj bok a: "))
+bok_b = int(input("Podaj bok b: "))
+bok_c = int(input("Podaj bok c: "))
+
+if ((bok_a + bok_b) > bok_c) & ((bok_a + bok_c) > bok_b) & ((bok_b + bok_c) > bok_a):
+    print("Da się zbudować trójkąt z podancyh boków.")
+    trojkat = [bok_a, bok_b, bok_c]
+    nadluzszy_bok = max(trojkat)
+    trojkat.remove(max(trojkat))
+    if ((trojkat[0] ** 2) + (trojkat[1] ** 2) == nadluzszy_bok ** 2):
+        print("Trójkąt prostokątny.")
+
+    print("Pole trójkąta wynosi: ", ((bok_a + bok_b + bok_c) / 2))
+    print("Obwód trójkąta wynosi: ", (bok_a + bok_b + bok_c))
+else:
+    print("Nie da się zbudować trójkąta.")
 
 # 3.2
 
@@ -60,30 +59,32 @@
 #         for x in range(65, 91):
 #             print(chr(x + 32) + chr(x), end="")
 
-import random
-
-n = int(input("Podaj ilość liczb: "))
-
-lista_liczb = []
-for x in range(0, n):
-    lista_liczb.append(random.randint(0, 99))
-
-print("Lista z indeksami.")
-for x, y in enumerate(lista_liczb):
-    print("[", x, "]", y, end=" ")
-
-print("\nLista w odwróconej kolejności.")
-for x in reversed(lista_liczb):
-    print(x, end=" ")
-
-print("\nLista uporządkowana.")
-for x in sorted((lista_liczb)):
-    print(x, end=" ")
-
-liczba_do_usuniecia = int(input("\nJaką liczbę usunąć z listy."))
-lista_liczb.remove(liczba_do_usuniecia)
-
-# if lista_liczb.index(liczba_do_usuniecia)==True:
+# import random
+#
+# n = int(input("Podaj ilość liczb: "))
+#
+# lista_liczb = []
+# for x in range(0, n):
+#     lista_liczb.append(random.randint(0, 99))
+#
+# print("Lista z indeksami.")
+# for x, y in enumerate(lista_liczb):
+#     print("[", x, "]", y, end=" ")
+#
+# print("\nLista w odwróconej kolejności.")
+# for x in reversed(lista_liczb):
+#     print(x, end=" ")
+#
+# print("\nLista uporządkowana.")
+# for x in sorted((lista_liczb)):
+#     print(x, end=" ")
+#
+# liczba_do_usuniecia = int(input("\nJaką liczbę usunąć z listy: "))
+#
+# if liczba_do_usuniecia in lista_liczb:
 #     lista_liczb.remove(liczba_do_usuniecia)
 # else:
-#     print("Brak elementu w liście")
+#     print("Nie ma takiej liczby w liście.")
+
+
+
