899a5a7c7bbc30b56faf7b03f429b2b3b4f787b5
Pc <yemeth44@gmail.com>
2018-12-30 01:33:36 +0100

    j

--- Lab1.py
+++ Lab1.py
@@ -1,3 +1,10 @@
+# Zadanie 1
+#
+# import os.path
+#
+# DIR = 'C:\dev'
+# print (len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
+#
 # Zadanie 2
 #
 # szyfr = input("Podaj szyfr: ")
@@ -7,9 +14,32 @@
 #     podany_szyfr = input("Podany szyfr jest błędny.\nPodaj szyfr ponownie: ")
 #
 # print("Podano popawny szyfr.")
+#
+# Zadanie 3
+#
+# import os.path
+#
+# DIR = 'C:\dev'
+#
+# for path, subdirs, files in os.walk(DIR):
+#     for name in files:
+#         print (os.path.join(DIR, name))
+#
+# Zadanie 4
+#
+# import os
+#
+# DIR = 'C:\dev'
+#
+# for filename in os.listdir(DIR):
+#        infilename = os.path.join(DIR,filename)
+#        if not os.path.isfile(infilename): continue
+#        oldbase = os.path.splitext(filename)
+#        newname = infilename.replace('.jpg', '.png')
+#        output = os.rename(infilename, newname)
 
 # Zadanie 5
-
+#
 # import math
 #
 # a = float(input("Podaj a: "))
@@ -55,9 +85,34 @@
 # sortowanie(lista_liczb)
 # print(lista_liczb)
 
+# fin = open('C:\dev\przed.txt')
+# delete_list = ['się', 'i', 'oraz', 'nigdy', 'dlaczego']
+# for line in fin:
+#     for word in delete_list:
+#         line = line.replace(word, "")
+# fin.close()
 
-import numpy as np
+#-*- coding: utf-8 -*-
 
-x = range(16384)
-x = np.reshape(x, (128, 128))
-print(x)
+inputfile = 'C:\dev\przed.txt'
+outputfile = 'C:\dev\po.txt'
+
+def remove_strings():
+    delete_list = ['się',' i','oraz','nigdy','dlaczego']
+    filein = open(inputfile)
+    fileout = open(outputfile, "w+")
+    for line in filein:
+        for word in delete_list:
+            line = line.replace(word, "")
+        fileout.write(line)
+    filein.close()
+    fileout.close()
+
+if __name__ == '__main__':
+    remove_strings()
+
+# import numpy as np
+#
+# x = range(16384)
+# x = np.reshape(x, (128, 128))
+# print(x)
