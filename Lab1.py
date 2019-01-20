da6211cd5e7eb99cade702049bdc2941de2dccfd
Pc <yemeth44@gmail.com>
2019-01-20 04:47:55 +0100

    j

--- Lab1.py
+++ Lab1.py
@@ -1,120 +1,112 @@
 # Zadanie 1
-#
-# import os.path
-#
-# DIR = 'C:\dev'
-# print (len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
-#
+
+import os.path
+
+DIR = 'C:\dev'
+print (len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
+
 # Zadanie 2
-#
-# szyfr = input("Podaj szyfr: ")
-# podany_szyfr = input("Aby przejść dalej podaj szyfr: ")
-#
-# while podany_szyfr != szyfr:
-#     podany_szyfr = input("Podany szyfr jest błędny.\nPodaj szyfr ponownie: ")
-#
-# print("Podano popawny szyfr.")
-#
+
+szyfr = input("Podaj szyfr: ")
+podany_szyfr = input("Aby przejść dalej podaj szyfr: ")
+
+while podany_szyfr != szyfr:
+    podany_szyfr = input("Podany szyfr jest błędny.\nPodaj szyfr ponownie: ")
+
+print("Podano popawny szyfr.")
+
 # Zadanie 3
-#
-# import os.path
-#
-# DIR = 'C:\dev'
-#
-# for path, subdirs, files in os.walk(DIR):
-#     for name in files:
-#         print (os.path.join(DIR, name))
-#
+
+import os.path
+
+DIR = 'C:\dev'
+
+for path, subdirs, files in os.walk(DIR):
+    for name in files:
+        print (os.path.join(DIR, name))
+
 # Zadanie 4
-#
-# import os
-#
-# DIR = 'C:\dev'
-#
-# for filename in os.listdir(DIR):
-#        infilename = os.path.join(DIR,filename)
-#        if not os.path.isfile(infilename): continue
-#        oldbase = os.path.splitext(filename)
-#        newname = infilename.replace('.jpg', '.png')
-#        output = os.rename(infilename, newname)
+
+import os
+
+DIR = 'C:\dev'
+
+for filename in os.listdir(DIR):
+       infilename = os.path.join(DIR,filename)
+       if not os.path.isfile(infilename): continue
+       oldbase = os.path.splitext(filename)
+       newname = infilename.replace('.jpg', '.png')
+       output = os.rename(infilename, newname)
 
 # Zadanie 5
-#
-# import math
-#
-# a = float(input("Podaj a: "))
-# while a == 0:
-#     print("Wartość a musi być różna od zera.")
-#     a = float(input("Podaj a: "))
-#
-# b = float(input("Podaj b: "))
-# c = float(input("Podaj c: "))
-#
-# delta = b * b - 4 * (a * c)
-#
-# if delta == 0:
-#
-#     x = -b / (2 * a)
-#     print("Miejsce zerowe to: ", x)
-#
-# elif delta < 0:
-#     print("Delta <0 brak miejsc zerowych.")
-# else:
-#     x1 = (-b - math.sqrt(delta)) / (2 * a)
-#     x2 = (-b + math.sqrt(delta)) / (2 * a)
-#     print("Miejsca zerowe x1 = {}, x2 = {}".format(x1, x2))
+
+import math
+
+a = float(input("Podaj a: "))
+while a == 0:
+    print("Wartość a musi być różna od zera.")
+    a = float(input("Podaj a: "))
+
+b = float(input("Podaj b: "))
+c = float(input("Podaj c: "))
+
+delta = b * b - 4 * (a * c)
+
+if delta == 0:
+
+    x = -b / (2 * a)
+    print("Miejsce zerowe to: ", x)
+
+elif delta < 0:
+    print("Delta <0 brak miejsc zerowych.")
+else:
+    x1 = (-b - math.sqrt(delta)) / (2 * a)
+    x2 = (-b + math.sqrt(delta)) / (2 * a)
+    print("Miejsca zerowe x1 = {}, x2 = {}".format(x1, x2))
 
 # Zadanie 6
-#
-# import random
-#
-# lista_liczb = []
-#
-# for x in range(0, 50):
-#     lista_liczb.append(random.randint(1, 99))
-#
-#
-# def sortowanie(lista):
-#     n = len(lista)
-#     for i in range(n):
-#         for j in range(0, n - i - 1):
-#             if lista[j] < lista[j + 1]:
-#                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
-#
-#
-# sortowanie(lista_liczb)
-# print(lista_liczb)
 
-# fin = open('C:\dev\przed.txt')
-# delete_list = ['się', 'i', 'oraz', 'nigdy', 'dlaczego']
-# for line in fin:
-#     for word in delete_list:
-#         line = line.replace(word, "")
-# fin.close()
+import random
+
+lista_liczb = []
+
+for x in range(0, 50):
+    lista_liczb.append(random.randint(1, 99))
+
+
+def sortowanie(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if lista[j] < lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+
+sortowanie(lista_liczb)
+print(lista_liczb)
+
+fin = open('C:\dev\przed.txt')
+delete_list = ['się', 'i', 'oraz', 'nigdy', 'dlaczego']
+for line in fin:
+    for word in delete_list:
+        line = line.replace(word, "")
+fin.close()
 
 # Zadanie 7
 
-# inputfile = 'C:\dev\przed.txt'
-# outputfile = 'C:\dev\po.txt'
-#
-# def remove_strings():
-#     delete_list = ['się',' i','oraz','nigdy','dlaczego']
-#     filein = open(inputfile)
-#     fileout = open(outputfile, "w+")
-#     for line in filein:
-#         for word in delete_list:
-#             line = line.replace(word, "")
-#         fileout.write(line)
-#     filein.close()
-#     fileout.close()
-#
-# if __name__ == '__main__':
-#     remove_strings()
+inputfile = 'C:\dev\przed.txt'
+outputfile = 'C:\dev\po.txt'
 
+def remove_strings():
+    delete_list = ['się',' i','oraz','nigdy','dlaczego']
+    filein = open(inputfile)
+    fileout = open(outputfile, "w+")
+    for line in filein:
+        for word in delete_list:
+            line = line.replace(word, "")
+        fileout.write(line)
+    filein.close()
+    fileout.close()
 
-
-# import numpy as np
-#
-# x = range(16384)
-# x = np.reshape(x, (128, 128))
-# print(x)
+if __name__ == '__main__':
+    remove_strings()
--- Lab2.py
+++ Lab2.py
@@ -1,125 +1,125 @@
 # Zadanie 1.1
-#
-# liczba_porzadkowa = int(input("Podaj liczbę porządkową: "))
-# liczba_koncowa = int(input("Podaj liczbę końcową: "))
-# wielkosc_odstepu = int(input("Podaj wielkość odstępu: "))
-#
-# for x in range(liczba_porzadkowa, liczba_koncowa, wielkosc_odstepu):
-#     print(x, end=" ")
+
+liczba_porzadkowa = int(input("Podaj liczbę porządkową: "))
+liczba_koncowa = int(input("Podaj liczbę końcową: "))
+wielkosc_odstepu = int(input("Podaj wielkość odstępu: "))
+
+for x in range(liczba_porzadkowa, liczba_koncowa, wielkosc_odstepu):
+    print(x, end=" ")
 
 # Zadanie 1.2
-#
-# komunikat = input("Wpisz swój komunikat: ")
-#
-# print(komunikat[::-1])
+
+komunikat = input("Wpisz swój komunikat: ")
+
+print(komunikat[::-1])
 
 
 # Zadanie 2.3
-#
-# wiadomosc = input("Wpisz wiadomość do zaszyfrowania: ")ś
-#
-# klucz = 3
-#
-# def szyfrowanie(wiadomosc):
-#     zaszyfrowana_wiadomosc = ""
-#     for x in range(len(wiadomosc)):
-#         if ord(wiadomosc[x]) > 122 - klucz:
-#             zaszyfrowana_wiadomosc += chr(ord(wiadomosc[x]) + klucz - 26)
-#         else:
-#             zaszyfrowana_wiadomosc += chr(ord(wiadomosc[x]) + klucz)
-#     return zaszyfrowana_wiadomosc
-#
-#
-# print("Zaszyfrowana wiadomość:\n", szyfrowanie(wiadomosc))
-#
+
+wiadomosc = input("Wpisz wiadomość do zaszyfrowania: ")ś
+
+klucz = 3
+
+def szyfrowanie(wiadomosc):
+    zaszyfrowana_wiadomosc = ""
+    for x in range(len(wiadomosc)):
+        if ord(wiadomosc[x]) > 122 - klucz:
+            zaszyfrowana_wiadomosc += chr(ord(wiadomosc[x]) + klucz - 26)
+        else:
+            zaszyfrowana_wiadomosc += chr(ord(wiadomosc[x]) + klucz)
+    return zaszyfrowana_wiadomosc
+
+
+print("Zaszyfrowana wiadomość:\n", szyfrowanie(wiadomosc))
+
 
 # Zadanie 3.1
 
-# import math
-#
-# bok_a = int(input("Podaj bok a: "))
-# bok_b = int(input("Podaj bok b: "))
-# bok_c = int(input("Podaj bok c: "))
-#
-# if ((bok_a + bok_b) > bok_c) & ((bok_a + bok_c) > bok_b) & ((bok_b + bok_c) > bok_a):
-#     print("Da się zbudować trójkąt z podancyh boków.")
-#     trojkat = [bok_a, bok_b, bok_c]
-#     nadluzszy_bok = max(trojkat)
-#     trojkat.remove(max(trojkat))
-#     if ((trojkat[0] ** 2) + (trojkat[1] ** 2) == nadluzszy_bok ** 2):
-#         print("Trójkąt prostokątny.")
-#     Obwod = bok_a + bok_b + bok_c
-#     p = Obwod / 2
-#     z = (p * (p - bok_a) * (p - bok_b) * (p - bok_c))
-#     Pole = math.sqrt(z)
-#     print("Obwód trójkąta wynosi: ", Obwod)
-#     print("Pole trójkąta wynosi: ", Pole)
-#
-# else:
-#     print("Nie da się zbudować trójkąta.")
+import math
+
+bok_a = int(input("Podaj bok a: "))
+bok_b = int(input("Podaj bok b: "))
+bok_c = int(input("Podaj bok c: "))
+
+if ((bok_a + bok_b) > bok_c) & ((bok_a + bok_c) > bok_b) & ((bok_b + bok_c) > bok_a):
+    print("Da się zbudować trójkąt z podancyh boków.")
+    trojkat = [bok_a, bok_b, bok_c]
+    nadluzszy_bok = max(trojkat)
+    trojkat.remove(max(trojkat))
+    if ((trojkat[0] ** 2) + (trojkat[1] ** 2) == nadluzszy_bok ** 2):
+        print("Trójkąt prostokątny.")
+    Obwod = bok_a + bok_b + bok_c
+    p = Obwod / 2
+    z = (p * (p - bok_a) * (p - bok_b) * (p - bok_c))
+    Pole = math.sqrt(z)
+    print("Obwód trójkąta wynosi: ", Obwod)
+    print("Pole trójkąta wynosi: ", Pole)
+
+else:
+    print("Nie da się zbudować trójkąta.")
 
 # Zadanie 3.2
 
-# for x in range(65, 91):
-#     print(chr(x) + chr(x + 32), end="")
-#     if x == 90:
-#         print("")
-#         for x in range(65, 91):
-#             print(chr(x + 32) + chr(x), end="")
+for x in range(65, 91):
+    print(chr(x) + chr(x + 32), end="")
+    if x == 90:
+        print("")
+        for x in range(65, 91):
+            print(chr(x + 32) + chr(x), end="")
 
 # Zadanie 3.3
 
-# import random
-#
-# n = int(input("Podaj ilość liczb: "))
-#
-# lista_liczb = []
-# for x in range(0, n):
-#     lista_liczb.append(random.randint(0, 99))
-#
-# print("Lista z indeksami.")
-# for x, y in enumerate(lista_liczb):
-#     print("[", x, "]", y, end=" ")
-#
-# print("\nLista w odwróconej kolejności.")
-# for x in reversed(lista_liczb):
-#     print(x, end=" ")
-# lista_liczb.reverse()
-#
-# print("\nLista uporządkowana.")
-# for x in sorted((lista_liczb)):
-#     print(x, end=" ")
-#
-# liczba_do_usuniecia = int(input("\nJaką liczbę usunąć z listy: "))
-#
-# try:
-#     lista_liczb.remove(liczba_do_usuniecia)
-#     print(lista_liczb)
-# except:
-#     print("Nie ma takiej liczby w liście.")
-#
-# indeks_do_usuniecia = int(input("\nPodaj numer indeksu liczby do usunięcia: "))
-#
-# try:
-#     lista_liczb.pop(indeks_do_usuniecia)
-#     print(lista_liczb)
-# except:
-#     print("Liczby o takim indeksie nie ma w liście.")
-#
-# liczba_do_sprawdzenia = int(input("\nPodaj liczbę do sprawdzenia: "))
-#
-# try:
-#     ile = lista_liczb.count(liczba_do_sprawdzenia)
-#     print("\nLiczba wystąpień elementu w liście: ", ile)
-# except:
-#     print("Nie ma takiej liczby w liście.")
-#
-# kiedy = lista_liczb.index(liczba_do_sprawdzenia)
-# print("\nNumer indeksu liczby w liście: ", kiedy)
-#
-# print("Indeksy od i do j.")
-#
-# i = int(input("\nPodaj i: "))
-# j = int(input("\nPodaj j: "))
-#
-# print("Elementy listy od i do j:",lista_liczb[i:j])
+import random
+
+n = int(input("Podaj ilość liczb: "))
+
+lista_liczb = []
+for x in range(0, n):
+    lista_liczb.append(random.randint(0, 99))
+
+print("Lista z indeksami.")
+for x, y in enumerate(lista_liczb):
+    print("[", x, "]", y, end=" ")
+
+print("\nLista w odwróconej kolejności.")
+for x in reversed(lista_liczb):
+    print(x, end=" ")
+lista_liczb.reverse()
+
+print("\nLista uporządkowana.")
+for x in sorted((lista_liczb)):
+    print(x, end=" ")
+
+liczba_do_usuniecia = int(input("\nJaką liczbę usunąć z listy: "))
+
+try:
+    lista_liczb.remove(liczba_do_usuniecia)
+    print(lista_liczb)
+except:
+    print("Nie ma takiej liczby w liście.")
+
+indeks_do_usuniecia = int(input("\nPodaj numer indeksu liczby do usunięcia: "))
+
+try:
+    lista_liczb.pop(indeks_do_usuniecia)
+    print(lista_liczb)
+except:
+    print("Liczby o takim indeksie nie ma w liście.")
+
+liczba_do_sprawdzenia = int(input("\nPodaj liczbę do sprawdzenia: "))
+
+try:
+    ile = lista_liczb.count(liczba_do_sprawdzenia)
+    print("\nLiczba wystąpień elementu w liście: ", ile)
+except:
+    print("Nie ma takiej liczby w liście.")
+
+kiedy = lista_liczb.index(liczba_do_sprawdzenia)
+print("\nNumer indeksu liczby w liście: ", kiedy)
+
+print("Indeksy od i do j.")
+
+i = int(input("\nPodaj i: "))
+j = int(input("\nPodaj j: "))
+
+print("Elementy listy od i do j:",lista_liczb[i:j])
